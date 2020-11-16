0476c70315fb782dde8f6783040fc25571c1a7c8
shakeib98 <shakeib98@outlook.com>
2020-11-16 14:29:10 +0500

    remvoed require condition in poolERC20

--- contracts/pool/contracts/PoolERC20.sol
+++ contracts/pool/contracts/PoolERC20.sol
@@ -93,8 +93,7 @@ contract PoolERC20 is IERC20 {
         address to,
         uint256 value
     ) private {
-        require(to != address(this) && to != address(0), "xFlashToken:: RECEIVER_IS_TOKEN_OR_ZERO");
-
+        require(to != address(0), "xFlashToken:: RECEIVER_IS_TOKEN_OR_ZERO");
         // Balance is implicitly checked with SafeMath's underflow protection
         balanceOf[from] = balanceOf[from].sub(value);
         balanceOf[to] = balanceOf[to].add(value);
